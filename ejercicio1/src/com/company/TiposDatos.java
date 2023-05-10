03095beafcdf5fe5d052646ffc45710706d6476a
cristiandibarrac <cristiand.ibarrac@gmail.com>
2023-05-10 09:59:40 -0500

    Prueba

--- ejercicio1/src/com/company/TiposDatos.java
+++ ejercicio1/src/com/company/TiposDatos.java
@@ -5,6 +5,6 @@ public class TiposDatos {
     public static void main(String[] args) {
         System.out.println("Los tipos de datos en JAVA son: ");
         System.out.println(" -Byte \n -Short \n -Int \n -Long \n -Float \n -Double \n -Char \n -String \n -Boolean");
-        System.out.println("Comentario de prueba");
+        System.out.println("Comentario de prueba 2");
     }
 }
