afeb3dc013a01248a19ab164773393724139404f
cristiandibarra <cristiand.ibarra@udea.edu.co>
2023-05-09 21:33:40 -0500

    Imprimir tipos de datos

---
+++ ejercicio1/src/com/company/TiposDatos.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class TiposDatos {
+
+    public static void main(String[] args) {
+        System.out.println("Los tipos de datos en JAVA son: ");
+        System.out.println(" -Byte \n -Short \n -Int \n -Long \n -Float \n -Double \n -Char \n -String \n -Boolean");
+
+    }
+}
